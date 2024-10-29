eaebd08804eaeff3120ddf708864442daea3919b
AkhlaqurRahmanOmi <akhlaqur.rahman.omi@gmaail.com>
2024-10-30 03:12:07 +0600

    Product router setup done

--- api/src/index.ts
+++ api/src/index.ts
@@ -1,5 +1,7 @@
-import express from 'express'
+import express, {Router} from 'express'
 
+
+import productRouters from './routes/products/index'
 
 const port = 3000;
 const app = express()
@@ -7,6 +9,11 @@ const app = express()
 app.get('/',(req, res) =>{
     res.send('Hello world')
 })
+
+
+
+
+app.use('/products',productRouters)
 
 app.listen(port, ()=>{
     console.log(`app is listing on port ${port}`)
