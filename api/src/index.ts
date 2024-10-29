d493913d4ed8068813f4c5f828046bc546aaaf98
AkhlaqurRahmanOmi <akhlaqur.rahman.omi@gmaail.com>
2024-10-30 04:30:52 +0600

    added json parsing middleware

--- api/src/index.ts
+++ api/src/index.ts
@@ -1,18 +1,12 @@
-import express, {Router} from 'express'
+import express, {json, urlencoded} from 'express'
 
 
 import productRouters from './routes/products/index'
 
 const port = 3000;
 const app = express()
-
-app.get('/',(req, res) =>{
-    res.send('Hello world')
-})
-
-
-
-
+app.use(urlencoded({extended:false}))
+app.use(json())
 app.use('/products',productRouters)
 
 app.listen(port, ()=>{
