60e168c294f53272f41dc3aa6d33639422b66fde
AkhlaqurRahmanOmi <akhlaqur.rahman.omi@gmaail.com>
2024-10-30 14:48:07 +0600

    just use prettier

--- api/src/routes/products/Controller.ts
+++ api/src/routes/products/Controller.ts
@@ -1,69 +1,78 @@
-import {Request, Response} from 'express'
-import { db } from '../../db/index'
-import { productsTable } from '../../db/ProductSchema'
-import { eq } from 'drizzle-orm'
+import { Request, Response } from "express";
+import { db } from "../../db/index";
+import { productsTable } from "../../db/ProductSchema";
+import { eq } from "drizzle-orm";
 
-
-export  async function listProduct(req: Request, res: Response){
-    try{
-        const products = await db.select().from(productsTable)
-        res.json(products)
-    } catch(e){
-        res.status(500).send(e)
-    }
+export async function listProduct(req: Request, res: Response) {
+  try {
+    const products = await db.select().from(productsTable);
+    res.json(products);
+  } catch (e) {
+    res.status(500).send(e);
+  }
 }
 
-export  async function getProductById(req: Request, res: Response){
-    const {id} = req.params;
-    try{
-        const [product] = await db.select().from(productsTable).where(eq(productsTable.id, Number(id)))
-        if(!product){
-            res.status(404).send({message: 'Product not found'})
-        }
-        else{
-            res.json(product)
-        }
-    } catch(e){
-        res.status(500).send(e)
+export async function getProductById(req: Request, res: Response) {
+  const { id } = req.params;
+  try {
+    const [product] = await db
+      .select()
+      .from(productsTable)
+      .where(eq(productsTable.id, Number(id)));
+    if (!product) {
+      res.status(404).send({ message: "Product not found" });
+    } else {
+      res.json(product);
     }
+  } catch (e) {
+    res.status(500).send(e);
+  }
 }
 
-export  async function createProduct(req: Request, res: Response){
-    try{
-        const [product] =await  db.insert(productsTable).values(req.body).returning()
-        res.status(201).json(product)
-    } catch(e) {
-        res.status(500).send(e)
-    }
+export async function createProduct(req: Request, res: Response) {
+  try {
+    const [product] = await db
+      .insert(productsTable)
+      .values(req.body)
+      .returning();
+    res.status(201).json(product);
+  } catch (e) {
+    res.status(500).send(e);
+  }
 }
 
-export  async function updateProduct(req: Request, res: Response){
-    const {id} = req.params
-    const updateFields = req.body
-    try{
-        const [product] = await db.update(productsTable).set(updateFields).where(eq(productsTable.id,Number(id))).returning()
-        if(product){
-            res.json(product)
-        }
-        else{
-            res.status(404).send({message: 'Product not found'})
-        }
-    } catch(e){
-        res.status(500).send(e)
+export async function updateProduct(req: Request, res: Response) {
+  const { id } = req.params;
+  const updateFields = req.body;
+  try {
+    const [product] = await db
+      .update(productsTable)
+      .set(updateFields)
+      .where(eq(productsTable.id, Number(id)))
+      .returning();
+    if (product) {
+      res.json(product);
+    } else {
+      res.status(404).send({ message: "Product not found" });
     }
+  } catch (e) {
+    res.status(500).send(e);
+  }
 }
 
-export  async function deleteProduct(req: Request, res: Response){
-    const {id} = req.params
-    try{
-        const [deletedItem]=await db.delete(productsTable).where(eq(productsTable.id,Number(id))).returning()
-        if(deletedItem){
-            res.status(204).send()
-        }
-        else{
-            res.status(404).send({message: 'Product not found'})
-        }
-    } catch(e){
-        res.status(500).send(e)
+export async function deleteProduct(req: Request, res: Response) {
+  const { id } = req.params;
+  try {
+    const [deletedItem] = await db
+      .delete(productsTable)
+      .where(eq(productsTable.id, Number(id)))
+      .returning();
+    if (deletedItem) {
+      res.status(204).send();
+    } else {
+      res.status(404).send({ message: "Product not found" });
     }
+  } catch (e) {
+    res.status(500).send(e);
+  }
 }
