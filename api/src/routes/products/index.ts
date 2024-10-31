32d2eb5844fa714b5ebd21181f496b7e0a93e390
AkhlaqurRahmanOmi <akhlaqur.rahman.omi@gmaail.com>
2024-10-31 15:50:35 +0600

    initialize the Zod with the drizzle ORM and it's 50 % complete

--- api/src/routes/products/index.ts
+++ api/src/routes/products/index.ts
@@ -1,12 +1,19 @@
 import { Router } from "express";
+import { createInsertSchema, createSelectSchema } from "drizzle-zod";
+import {z}  from 'zod'
+
 
 import { listProduct, getProductById, createProduct,updateProduct,deleteProduct } from "./Controller";
+import { validateData } from "../../middlewares/validationMiddleware";
+import { productsTable } from "../../db/ProductSchema";
+
+const creatProductSchema = createInsertSchema(productsTable)
 
 // product endpoint
 const router = Router()
 router.get('/',listProduct)
 router.get('/:id', getProductById)
-router.post('/', createProduct)
+router.post('/',validateData(creatProductSchema), createProduct)
 router.put('/:id', updateProduct)
 router.delete('/:id', deleteProduct)
 export default router
