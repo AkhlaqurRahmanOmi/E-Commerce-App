0c6e6a74ef4023e2e2811adf3e6762f7437fc0a6
AkhlaqurRahmanOmi <akhlaqur.rahman.omi@gmaail.com>
2024-11-10 00:09:00 +0600

    order crud done without delete operation

--- api/src/routes/orders/index.ts
+++ api/src/routes/orders/index.ts
@@ -1,11 +1,13 @@
 import { Router } from "express";
-import { createOrder } from "./controller.js";
+import { createOrder, getOrder, listOrders, updateOrder } from "./controller.js";
 import { validateData } from "../../middlewares/validationMiddleware.js";
-import { insertOrderSchema, insertOrderWithItemsSchema } from "../../db/ordersSchema.js";
+import { insertOrderSchema, insertOrderWithItemsSchema, updateOrderSchema } from "../../db/ordersSchema.js";
 import { verifyToken } from "../../middlewares/authMiddleware.js";
 
 const router = Router()
 
 router.post('/',verifyToken,validateData(insertOrderWithItemsSchema),createOrder)
-
+router.get("/", verifyToken, listOrders);
+router.get('/:id',verifyToken, getOrder)
+router.put('/id',verifyToken,validateData(updateOrderSchema),updateOrder)
 export default router
