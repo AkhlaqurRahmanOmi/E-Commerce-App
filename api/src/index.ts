4afb86b8ca6ec8bdb9f8970ceb6e186f91a552c6
AkhlaqurRahmanOmi <akhlaqur.rahman.omi@gmaail.com>
2024-11-09 23:43:34 +0600

    order and order items create operation done

--- api/src/index.ts
+++ api/src/index.ts
@@ -3,6 +3,7 @@ import serverless from "serverless-http";
 
 import productRouters from './routes/products/index.js'
 import authRoutes from './routes/auth/index.js'
+import ordersRoutes from './routes/orders/index.js'
 
 const port = 3000;
 const app = express()
@@ -10,6 +11,7 @@ app.use(urlencoded({extended:false}))
 app.use(json())
 app.use('/products',productRouters)
 app.use('/auth',authRoutes)
+app.use('/orders',ordersRoutes)
 
  if (process.env.NODE_ENV === "dev"){
      app.listen(port, () => {
